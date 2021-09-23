5aaa5584619efcd6081d7074a8a39521125d174e
Yurnero99 <azwad.sidrat@gmail.com>
2021-09-23 13:22:23 +0600

    Starting

--- Scripts_For_Myself/My_zoom_class/work.py
+++ Scripts_For_Myself/My_zoom_class/work.py
@@ -1,4 +1,72 @@
 ######    Script For My Own Zoom Classes   ######
 
 
+from selenium import webdriver
+
+import pyperclip
+import time
+import os
+# __link = 'https://us04web.zoom.us/j/4534726949?pwd=YzhHYmNRT0ZVT3hjdEViQ29WTmVVdz09'
+# __meetingID = '453 472 6949'
+# __passcode = '00001111'
+# __messenger = 'https://www.messenger.com/'
+
+
+
+class Zoom:
+
+
+    def __init__(self,username,password):
+        
+        #Login info
+        self.username = username
+        self.password = password
+
+
+        #Driver
+        driver_path = os.path.join(os.getcwd() , 'chromedriver.exe')
+        self.driver = webdriver.Chrome(executable_path=driver_path)
+
+
+
+
+    def start_zoom(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main(username,password):
+    jarvis = Zoom(username,password)
+    jarvis.login()
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    username = input("Enter your usename\n")
+    password = input("Enter your password\n")
+    main(username,password)
+
 
